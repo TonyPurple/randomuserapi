afcef2a594bb53c8a7fae6125ce8b3648bd61c11
Anthony Vanoni <tony@wrestlingwithcode.com>
2022-07-10 16:51:41 -0700

    remove unnecessary files

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Lists from "./components/List";
-import SearchBar from "./components/SearchBar";
-import './App.css'
+import Lists from "./List";
+import SearchBar from "./SearchBar";
+import '../App.css'
  
 const App = () => {
   const [users, setUsers] = useState([]);
