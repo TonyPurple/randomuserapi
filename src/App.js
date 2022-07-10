4052b19098eb5fe2e6d65c9e838c1106493ee93d
Anthony Vanoni <tony@wrestlingwithcode.com>
2022-07-10 16:15:28 -0700

    fetch data with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
  
 const App = () => {
   const [users, setUsers] = useState([]);
   const [store, setStore] = useState([]);
+
+  const getUsers = () => {
+    axios.get("https://randomuser.me/api/?results=10&inc=name,registered&nat=fr")
+    .then(response => console.log(response.data.results[0].name.first))
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
  
   return (
     <div>
